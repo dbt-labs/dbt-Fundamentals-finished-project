514abb05e1ede1d26e5f93f7107acf9a0127353f
Lucas Silbernagel <lucas.silbernagel+old@dbtlabs.com>
2026-06-09 12:05:17 +0000

    add new data

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,28 +1,32 @@
-with orders as  (
-    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+
+with orders as (
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 ),
 
 payments as (
-    select * from {{ ref ('stg_stripe__payments') }}
+    select * from {{ ref('stg_stripe__payments') }}
 ),
 
 order_payments as (
     select
         order_id,
-        sum (case when status = 'success' then amount end) as amount
-
+        sum(case when status = 'success' then amount end) as amount
     from payments
     group by 1
 ),
 
- final as (
-
+final as (
     select
-        orders.order_id,
+        orders.order_id as order_idd,
         orders.customer_id,
         orders.order_date,
-        coalesce (order_payments.amount, 0) as amount
-
+        coalesce(order_payments.amount, 0) as amount
     from orders
     left join order_payments using (order_id)
 )
